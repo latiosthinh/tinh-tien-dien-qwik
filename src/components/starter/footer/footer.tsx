d8f0445b0f29e4ccf4a4f513fd941d052988d301
ScaryLewii <120034859+ScaryLewii@users.noreply.github.com>
2023-07-16 18:15:48 +0700

    remove unused dom in footer

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -2,13 +2,11 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <footer class="mt-auto w-full">
-      <div class="mx-auto py-4 text-center">
-        <a href="https://github.com/latiosthinh" target="_blank">
-          <span>&copy; Copyright 2023. </span>
-          <span>Made with ♡ by Thomas Nguyen</span>
-        </a>
-      </div>
+    <footer class="mt-auto w-full py-4 text-center">
+      <a href="https://github.com/latiosthinh" target="_blank">
+        <span>&copy; Copyright 2023. </span>
+        <span>Made with ♡ by Thomas Nguyen</span>
+      </a>
     </footer>
   );
 });
